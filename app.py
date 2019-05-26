6362ab0d2b923f45fd7d0e5138ef71580e175b0b
Tae Hyoung Jo <taehyoung.jo@gmail.com>
2019-05-26 14:17:27 -0400

    shell

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """"""
+    if request.method == "GET":
+        return render_template("index.html")
