2ed260084f646bdbfe2507025772ebbacc2c5925
Tae Hyoung Jo <taehyoung.jo@gmail.com>
2019-05-26 15:48:41 -0400

    Swiping works

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+
 app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/", methods=["GET", "POST"])
 def index():
